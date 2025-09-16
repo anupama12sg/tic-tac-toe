11d5cf707e390d0b2a96c8d4d209ed022054176b
Anu <anupama12sg@gmail.com>
2025-09-16 12:20:36 +0530

    chore: create title and button, div

--- src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -5,7 +5,13 @@ import zero_icon from '../zero.png'
 
 const TicTacToe = () => {
     return (
-        <div>TicTacToe</div>
+        <div className='container'>
+            <h1 className='title'>Tic Tac Toe in <span>React</span></h1>
+            <div className='board'>
+
+            </div>
+            <button className='reset'>Reset</button>
+        </div>
     )
 }
 
