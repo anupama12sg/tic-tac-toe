96d79cf7c9858f5e31e121ed8ac28ce71d2ef287
Anu <anupama12sg@gmail.com>
2025-09-22 13:03:13 +0530

    chore: conditions in toggle function

--- src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -5,14 +5,28 @@ import zero_icon from '../zero.png'
 
 let data = ["", "", "", "", "", "", "", "", ""]
 
-let [count, setCount] = useState(0);
-let [lock, setLock] = useState(false);
-
-const toggle = (w, num) => {
-
-}
-
 const TicTacToe = () => {
+
+    let [count, setCount] = useState(0);
+    let [lock, setLock] = useState(false);
+
+    const toggle = (w, num) => {
+        if (lock) {
+            return 0;
+        }
+        if (count % 2) {
+            e.target.innerHTML = `<img src='${cross_icon}'>`;
+            data[num] = "x";
+            setCount(++count);
+        }
+        else {
+            e.target.innerHTML = `<img src='${circle_icon}'>`;
+            data[num] = "o";
+            setCount(++count);
+        }
+    }
+
+
     return (
         <div className='container'>
             <h1 className='title'>Tic Tac Toe in <span> React</span></h1>
