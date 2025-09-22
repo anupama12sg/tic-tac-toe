36d7457987f58ad781cfab1913b8257ea5596eff
Anu <anupama12sg@gmail.com>
2025-09-22 13:48:54 +0530

    crrection

--- src/Components/Assets/TicTacToe/TicTacToe.jsx
+++ src/Components/Assets/TicTacToe/TicTacToe.jsx
@@ -14,7 +14,7 @@ const TicTacToe = () => {
         if (lock) {
             return 0;
         }
-        if (count % 2) {
+        if (count % 2 === 0) {
             e.target.innerHTML = `<img src='${cross_icon}'>`;
             data[num] = "x";
             setCount(++count);
